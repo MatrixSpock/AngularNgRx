d24d41ed59882330268f1c62a2176359df6fc074
Jose Ibarra <jibarra8917@gmail.com>
2022-09-20 16:21:15 -0600

    Fixing format

--- src/app/products/state/product.actions.ts
+++ src/app/products/state/product.actions.ts
@@ -2,26 +2,26 @@ import { createAction, props } from "@ngrx/store";
 import { Product } from "../product";
 
 export const toggleProductCode = createAction(
-    '[Product] Toggle Product Code'
-    );
+  '[Product] Toggle Product Code'
+);
 export const setCurrentProduct = createAction(
   '[Product] Set Current Product',
-  props<{currentProductId: number}>()
-  );
+  props<{ currentProductId: number }>()
+);
 export const clearCurrentProduct = createAction(
   '[Product] Clear Current Product'
-  );
+);
 export const initializeCurrentProduct = createAction(
   '[Product] Initialize Current Product'
-  );
+);
 export const loadProducts = createAction(
   '[Product] Load'
 );
 export const loadProductsSuccess = createAction(
   '[Product] Load Success',
-  props<{products: Product[]}>()
-  );
+  props<{ products: Product[] }>()
+);
 export const loadProductsFailure = createAction(
   '[Product] Load Fail',
-  props<{error: string}>()
+  props<{ error: string }>()
 );
