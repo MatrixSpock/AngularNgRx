652c2039c4bb270ec71f2dfa21312885fa8fff81
Jose Ibarra <jibarra8917@gmail.com>
2022-09-22 10:02:28 -0600

    CREATING REUSABLE SHELL COMPONENT - Transforming the product-list.component.ts into a presentational component by adding @Inputs and Event Emitters and moving data logic to the product-shell.component.ts. Replacing all observable variables in the product list component with just regular components and moving the observable variables defined in the template by the async operator to the product-shell component template instead.

--- src/app/products/product-list/product-list.component.ts
+++ src/app/products/product-list/product-list.component.ts
@@ -1,75 +1,35 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Store } from '@ngrx/store';
-
-import { Observable, Subscription } from 'rxjs';
-
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 import { Product } from '../product';
-import { getCurrentProduct, getError, getProducts, getShowProductCode, State } from '../state/product.reducer';
-import * as ProductActions from '../state/product.actions';
+
 
 @Component({
   selector: 'pm-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent {
   pageTitle = 'Products';
-  // errorMessage: string;
 
-  displayCode: boolean;
-
-  products$: Observable<Product[]>;
-  selectedProduct$: Observable<Product>;
-  displayCode$: Observable<boolean>;
-  errorMessage$: Observable<string>;
-  // sub: Subscription;
-
-  constructor(private store: Store<State>) { }
-
-  ngOnInit(): void {
-
-    // Original
-    // this.sub = this.productService.selectedProductChanges$.subscribe(
-    //   currentProduct => this.selectedProduct = currentProduct
-    // );
-
-    // TODO: Unsubscribe
-    //New
-    this.selectedProduct$ = this.store.select(getCurrentProduct);
-
-    /* New */
-    this.products$ = this.store.select(getProducts);
-    this.store.dispatch(ProductActions.loadProducts());
-
-    /* Original */
-    // this.productService.getProducts().subscribe({
-    //   next: (products: Product[]) => this.products = products,
-    //   error: err => this.errorMessage = err
-    // });
-
-    this.errorMessage$ = this.store.select(getError);
-
-    // TODO: Unsubscribe
-    this.displayCode$ = this.store.select(getShowProductCode)
-  }
-
-  // ngOnDestroy(): void {
-  //   this.sub.unsubscribe();
-  // }
+  @Input() errorMessage: string;
+  @Input() products: Product[];
+  @Input() displayCode: boolean;
+  @Input() selectedProduct: Product;
+  @Output() displayCodeChanged = new EventEmitter<void>();
+  @Output() initializeNewProduct = new EventEmitter<void>();
+  @Output() productWasSelected = new EventEmitter<Product>();
 
   checkChanged(): void {
-    // this.displayCode = !this.displayCode;
-    this.store.dispatch(ProductActions.toggleProductCode());
+    this.displayCodeChanged.emit();
   }
 
   newProduct(): void {
-    // this.productService.changeSelectedProduct(this.productService.newProduct());
-    this.store.dispatch(ProductActions.initializeCurrentProduct());
+    this.initializeNewProduct.emit();
   }
 
   productSelected(product: Product): void {
-    // this.productService.changeSelectedProduct(product);
-    this.store.dispatch(ProductActions.setCurrentProduct({ currentProductId: product.id }));
+    this.productWasSelected.emit(product);
   }
+
 
 }
